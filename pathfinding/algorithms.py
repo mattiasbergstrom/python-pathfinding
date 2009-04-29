5a9b15333629bafbe0365d36044daa70054e0b55
JeanpierreDA <JeanpierreDA@dd1093a6-2adc-11de-b2a4-4f96ceaaac44>
2009-04-29 14:41:23 +0000

    Resolved bug #1 (added astar()) Resolved bug #2 (added RectNode)
    Added these metrics: manhattan, chebyshev, euclidean, discrete
    Added these semimetrics: euclidean-squared
    Added these premetrics: zero
    
    Added these nodes: RectNode
    
    Added these algorithms: A*
    
    Added basic map loading
    Added test suite (uses nose), tests metrics completely, pathfinding and nodes partially.

---
+++ pathfinding/algorithms.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#py_algorithms.py
+"""This module stores the implementations of the pathfinding algorithms"""
+import heapq
+__all__ = ['astar']
+
+def astar(start_node, target_node):
+    """The A* pathfinding algorithm
+    """
+    # TODO: tie break f values with h (tuple (f, h, node), not (f, node))
+    # reduces searching in general, am I right?
+    closed = set()
+    open_set = set()
+    open = []
+    
+    h_score = {start_node: start_node.heuristic(target_node)}
+    g_score = {start_node: 0}
+    f_score = {start_node: h_score[start_node] + g_score[start_node]}
+    
+    came_from = {}
+    start_pair = [f_score[start_node], start_node]
+    heapq.heappush(open, start_pair)
+    open_d = {start_node: start_pair}
+    while open:
+        f, node = heapq.heappop(open)
+        del open_d[node]
+        if node == target_node:
+            return reconstruct_path(came_from, target_node) #fix <-- what did that mean?
+        closed.add(node)
+        for neighbor in node.get_neighbors():
+            if neighbor in closed:
+                continue
+         
+            tentative_g_score = g_score[node] + node.move_cost(neighbor)
+            if neighbor not in open_d:
+                came_from[neighbor] = node
+                g_score[neighbor] = tentative_g_score
+                h_score[neighbor] = neighbor.heuristic(target_node)
+                f = f_score[neighbor] = g_score[neighbor] + h_score[neighbor]
+                d = open_d[neighbor] = [f, neighbor]
+                heapq.heappush(open, d)
+            elif tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = node
+                g_score[neighbor] = tentative_g_score
+                f = f_score[neighbor] = g_score[neighbor] + h_score[neighbor]
+                open_d[neighbor][0] = f
+                heapq.heapify(open)
+                
+    
+    raise ValueError("No path exists.")
+
+def reconstruct_path(came_from, target_node):
+    path = []
+    node = target_node
+    while node in came_from:
+        path.append(node)
+        node = came_from[node]
+    return reversed(path)
---
+++ pathfinding/test/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import test_load_map
+import test_pathfinding
+import unittest
+import nose
+
+__all__ = ['test_load_map', 'test_pathfinding']
+    
+main = nose.main
